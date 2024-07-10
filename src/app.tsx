087a1ec4eae5d737957b28397145952c4931c0b5
Elias Costa <eliashead.costa@gmail.com>
2024-07-10 11:07:47 -0300

    remove email

--- src/app.tsx
+++ src/app.tsx
@@ -41,6 +41,12 @@ export function App() {
     event.currentTarget.reset()
   }
 
+  const removeEmailsFromInvites = (emailToRemove: string) => {
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+
+    setEmailsToInvite(newEmailList)
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -117,7 +123,7 @@ export function App() {
                 return (
                   <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
                   <span className='text-zinc-300'>{email}</span>
-                  <button type='button'>
+                  <button type='button' onClick={() => removeEmailsFromInvites(email)}>
                     <X className='size-4 text-zinc-400' />
                   </button>
                 </div>
